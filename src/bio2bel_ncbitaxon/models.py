a0cbd08b0f67156e7a65e9022046ea61ab40151b
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-02-19 08:31:12 +0100

    Add abstraction to pytaxtree models

---
+++ src/bio2bel_ncbitaxon/models.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Provide abstraction from :mod:`pytaxtree` database models"""
+
+from pytaxtree.manager.models import Citation, Division, GeneticCode, Name, Node
+
+__all__ = [
+    'Citation', 'Division', 'GeneticCode', 'Node', 'Name',
+]
